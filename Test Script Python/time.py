0a4cfb4c2feca473a55bd62c8ea2846545657ae5
DRAJAT PAMUJI <DRAJAT PAMUJI@IDEAPAD5-14ARE0>
2022-08-03 16:43:14 +0700

    update tc10 dan link google drive

--- Test Script Python/time.py
+++ Test Script Python/time.py
@@ -31,6 +31,34 @@ class Time(unittest.TestCase):
         response_data = browser.find_element(By.ID,"viewTimesheetForm").text
 
         self.assertIn('Garry White', response_data)
+
+    # submodul project info >> customer
+    def test_tc10_success_add_customer(self):
+        # steps
+        browser = self.browser
+        login.Login.test_tc01_success_login(self) # memanggil tc01 login
+        time.sleep(1)
+        browser.find_element(By.ID,"menu_time_viewTimeModule").click()
+        time.sleep(1)
+        browser.find_element(By.ID,"menu_admin_ProjectInfo").click()
+        time.sleep(1)
+        browser.find_element(By.ID,"menu_admin_viewCustomers").click()
+        time.sleep(1)
+        browser.find_element(By.ID,"btnAdd").click()
+        time.sleep(1)
+        browser.find_element(By.ID,"addCustomer_customerName").send_keys("Indonesia Eximbank")
+        time.sleep(1)
+        browser.find_element(By.ID,"addCustomer_description").send_keys("Lembaga Pembiayaan Ekspor Indonesia")
+        time.sleep(1)
+        browser.find_element(By.ID,"btnSave").click()
+        time.sleep(1)
+
+        # validasi
+        response_message = browser.find_element(By.CLASS_NAME,"fadable").text
+        response_data = browser.find_element(By.ID,"resultTable").text
+
+        self.assertEqual(response_message, 'Successfully Saved') 
+        self.assertIn('Indonesia Eximbank', response_data)
         
     def tearDown(self): 
         self.browser.close()
